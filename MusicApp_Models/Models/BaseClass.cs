e274941b1d6d7d5488c36a074225462f4ce3a9c7
Eleonora9900 <153208470+Eleonora9900@users.noreply.github.com>
2024-05-24 12:00:44 +0300

    BaseClass
    Added a base class.

---
+++ MusicApp_Models/Models/BaseClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MusicApp_Models.Models
+{
+	public class BaseClass
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; } = String.Empty;
+	}
+}
+
--- MusicApp_Models/Models/Song.cs
+++ MusicApp_Models/Models/Song.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 namespace MusicApp_Models.Models
 {
-	public class Song
+	public class Song : BaseClass
 	{
-		public Guid Id { get; set; }
-		public string Title { get; set; } = String.Empty;
 		public int ArtistId { get; set; } 
 		public int AlbumId { get; set; }
 		public string Genre { get; set; } = String.Empty;
