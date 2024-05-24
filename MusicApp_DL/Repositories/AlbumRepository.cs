5c445c933875eb52380de8baa1dba0b8fd7f0d84
Eleonora9900 <153208470+Eleonora9900@users.noreply.github.com>
2024-05-24 12:39:03 +0300

    Repositories
    Added song, artist and album repositories.

---
+++ MusicApp_DL/Repositories/AlbumRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MusicApp_DL.Interfaces;
+using MusicApp_DL.MemoryDB;
+using MusicApp_Models.Models;
+
+namespace MusicApp_DL.Repositories
+{
+    public class AlbumRepository : IAlbumRepository
+    {
+        public void Add(Album album)
+        {
+            InMemoryDB.AlbumsData.Add(album);
+        }
+
+        public void Delete(Guid id)
+        {
+            var album = GetById(id);
+            if (album != null)
+            {
+                InMemoryDB.AlbumsData.Remove(album);
+            }
+        }
+
+        public List<Album> GetAll()
+        {
+            return InMemoryDB.AlbumsData;
+        }
+
+        public Album GetById(Guid id)
+        {
+            return InMemoryDB.AlbumsData.FirstOrDefault(album => album.Id == id);
+        }
+    }
+}
+
---
+++ MusicApp_DL/Repositories/SongRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MusicApp_DL.Interfaces;
+using MusicApp_DL.MemoryDB;
+using MusicApp_Models.Models;
+
+namespace MusicApp_DL.Repositories
+{
+    public class SongRepository : ISongRepository
+    {
+        public void Add(Song song)
+        {
+            InMemoryDB.SongsData.Add(song);
+        }
+
+        public void Delete(Guid id)
+        {
+            var song = GetById(id);
+            if (song != null)
+            {
+                InMemoryDB.SongsData.Remove(song);
+            }
+        }
+
+        public List<Song> GetAll()
+        {
+            return InMemoryDB.SongsData;
+        }
+
+        public Song GetById(Guid id)
+        {
+            return InMemoryDB.SongsData.FirstOrDefault(song => song.Id == id);
+        }
+    }
+}
+
