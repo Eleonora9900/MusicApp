c66680ab89d7bde868c6b1320d137d97417793dd
Eleonora9900 <153208470+Eleonora9900@users.noreply.github.com>
2024-05-26 19:37:52 +0300

    Controllers
    Added controllers.

---
+++ MusicApp/Controllers/ArtistController.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MusicApp_BL.Interfaces;
+using MusicApp_Models.Models;
+
+namespace MusicApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArtistController : ControllerBase
+    {
+        private readonly IArtistService _artistService;
+        public ArtistController(IArtistService artistService)
+        {
+            _artistService = artistService;
+        }
+
+        [HttpGet("Get All")]
+        public IEnumerable<Artist> GetAll()
+        {
+            return _artistService.GetAll();
+        }
+
+        [HttpGet("GetById")]
+        public Artist GetById(int id)
+        {
+            return _artistService.GetById(id);
+        }
+
+        [HttpPost("Add")]
+        public void Add([FromBody] Artist artist)
+        {
+            _artistService.Add(artist);
+        }
+
+        [HttpDelete("Delete")]
+        public void Delete(int id)
+        {
+            _artistService.Delete(id);
+        }
+    }
+}
+
---
+++ MusicApp/Controllers/SongController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp_BL.Interfaces;
+using MusicApp_Models.Models;
+
+namespace MusicApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SongController : ControllerBase
+    {
+        private readonly ISongService _songService;
+
+        public SongController(ISongService songService)
+        {
+            _songService = songService;
+        }
+
+        [HttpGet("GetAll")]
+        public IEnumerable<Song> GetAll()
+        {
+            return _songService.GetAllSongs();
+        }
+
+        [HttpGet("GetById")]
+        public Song GetById(int id)
+        {
+            return _songService.GetById(id);
+        }
+
+        [HttpPost("Add")]
+        public void Add([FromBody] Song song)
+        {
+            _songService.AddSong(song);
+        }
+
+        [HttpDelete("Delete")]
+        public void Delete(int id)
+        {
+            _songService.DeleteSong(id);
+        }
+    }
+}
+
